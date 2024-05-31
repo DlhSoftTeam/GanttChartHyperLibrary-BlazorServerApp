0f599b357f0e8eb898755405493339aee0ab0e74
Sorin Dolha <sdolha@hotmail.com>
2024-05-31 20:21:57 +0300

    Added GCHL, some interop

---
+++ wwwroot/js/app.js
@@ -0,0 +1,17 @@
+﻿var ganttChartView;
+window.interop = {
+    initialize: () => {
+        ganttChartView = document.getElementById("ganttChartView");
+        DlhSoft.Controls.GanttChartView.initialize(ganttChartView,
+            [{ content: 'Item 1' }, { content: 'Item 2' }],
+            { itemPropertyChangeHandler: itemPropertyChanged });
+    },
+    addItem: (item) => {
+        ganttChartView.addItem(item);
+    }
+};
+function itemPropertyChanged(item, propertyName, isDirect, isFinal) {
+    if (!isDirect || !isFinal || propertyName == 'isSelected')
+        return;
+    alert('Property ' + propertyName + ' of item ' + item.content + ' has changed.')
+}
